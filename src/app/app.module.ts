b6f54035d0bb4a7dc3ac9a0e5a0877294f66fd12
Kevin Real <krealina24@hotmail.es>
2021-02-08 18:34:31 +0000

    Scheduler first approach. Dependencies installed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchedulerComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scheduler/scheduler.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-scheduler',
+  templateUrl: './scheduler.component.html',
+  styleUrls: ['./scheduler.component.scss']
+})
+export class SchedulerComponent implements OnInit {
+
+  numberCols = Array(7);
+  numberRows = Array(13);
+
+  cita1 = [
+    'Cita1'
+  ];
+  cita2 = [
+    'Cita2',
+    'Cita3'
+  ];
+
+  constructor() { }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
